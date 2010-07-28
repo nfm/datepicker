e88f4ca1c622ad6e6ad34fc0c02ec0ac2266b047
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-07-28 12:37:43 +1000

    Wrote initial uninstall.rb code to remove javascript, images, and stylesheets dir

--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,24 @@
-# Uninstall hook code here
+remove_file "public/javascripts/datepicker.min.js"
+remove_file "public/images/datepicker-icon.png"
+remove_dir "public/stylesheets/datepicker-styles/"
+puts "Files removed - uninstallation complete!"
+
+def remove_file(file)
+	puts "Removing #{file}"
+	begin
+		File.delete(file)
+	rescue Exception => e
+		puts "Unable to remove #{file}"
+		puts e
+	end
+end
+
+def remove_dir(dir)
+	puts "Removing #{dir}"
+	begin
+		FileUtils.rmdir(dir)
+	rescue Exception => e
+		puts "Unable to remove #{dir}"
+		puts e
+	end
+end
