864f3ca6b5d0c3fb8639a3101b445f27dc42e846
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-07-28 12:56:50 +1000

    Fixed copying of files in install.rb

--- install.rb
+++ install.rb
@@ -1,8 +1,8 @@
 puts "Copying files..."
 
 src_dir = File.join(File.dirname(__FILE__), "lib", "installed_files")
-FileUtils.copy(File.join(src_dir, "datepicker.min.js"), "#{RAILS_ROOT}/public/javascripts/")
-FileUtils.copy(File.join(src_dir, "datepicker-icon.png"), "#{RAILS_ROOT}/public/images/")
-FileUtils.copy(File.join(src_dir, "datepicker-styles"), "#{RAILS_ROOT}/public/stylesheets")
+FileUtils.copy(File.join(src_dir, "datepicker.min.js"), "#{RAILS_ROOT}/public/javascripts/datepicker.min.js")
+FileUtils.copy(File.join(src_dir, "datepicker-icon.png"), "#{RAILS_ROOT}/public/images/datepicker-icon.png")
+FileUtils.cp_r(File.join(src_dir, "datepicker-styles"), "#{RAILS_ROOT}/public/stylesheets/datepicker-styles")
 
 puts "Files copied - installation complete!"
