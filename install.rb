8a7aca5f64a005e23ff14c96e8d9478fe028aa7f
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-07-28 12:33:00 +1000

    Wrote initial install.rb code to copy in javascript, image, and stylesheets

--- install.rb
+++ install.rb
@@ -1,1 +1,8 @@
-# Install hook code here
+puts "Copying files..."
+
+src_dir = File.join(File.dirname(__FILE__), "lib", "installed_files")
+FileUtils.copy(File.join(src_dir, "datepicker.min.js"), "#{RAILS_ROOT}/public/javascripts/")
+FileUtils.copy(File.join(src_dir, "datepicker-icon.png"), "#{RAILS_ROOT}/public/images/")
+FileUtils.copy(File.join(src_dir, "datepicker-styles"), "#{RAILS_ROOT}/public/stylesheets")
+
+puts "Files copied - installation complete!"
