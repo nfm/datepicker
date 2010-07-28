22354102e5717bb562875b0cd375ad6e246a0363
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-07-28 13:17:01 +1000

    Copied datepicker-styles dir to public/stylesheets/datepicker/ in install.rb

--- install.rb
+++ install.rb
@@ -3,6 +3,6 @@ puts "Copying files..."
 src_dir = File.join(File.dirname(__FILE__), "lib", "installed_files")
 FileUtils.copy(File.join(src_dir, "datepicker.min.js"), "#{RAILS_ROOT}/public/javascripts/datepicker.min.js")
 FileUtils.copy(File.join(src_dir, "datepicker-icon.png"), "#{RAILS_ROOT}/public/images/datepicker-icon.png")
-FileUtils.cp_r(File.join(src_dir, "datepicker-styles"), "#{RAILS_ROOT}/public/stylesheets/datepicker-styles")
+FileUtils.cp_r(File.join(src_dir, "datepicker-styles"), "#{RAILS_ROOT}/public/stylesheets/datepicker")
 
 puts "Files copied - installation complete!"
